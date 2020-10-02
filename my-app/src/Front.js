9a7b240c01375cbb1cd4bddb4d8889fbf77c1724
alexavila150 <alexadrianavila@gmail.com>
2020-10-02 03:38:59 -0600

    modified padding in image

--- my-app/src/Front.js
+++ my-app/src/Front.js
@@ -28,7 +28,7 @@ function Front(){
                         </div>
                         
                     </Col>
-                    <Col sm={5}>
+                    <Col sm={5} className = "Image">
                         <img src="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/80203944_2859807094043966_3896791748550066176_o.jpg?_nc_cat=100&_nc_sid=09cbfe&_nc_ohc=0xtyNsRWOykAX9-701U&_nc_ht=scontent-dfw5-2.xx&oh=d6d018cebbebc7b16db0aeddfbfb7ec5&oe=5F7C700A"></img>
                     </Col>
                 </Row>
