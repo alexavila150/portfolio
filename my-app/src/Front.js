0b5faf2d683c44fcd200ffe61e16526d55f4f99b
alexavila150 <alexadrianavila@gmail.com>
2020-10-07 22:19:01 -0600

    bring picture to public

--- my-app/src/Front.js
+++ my-app/src/Front.js
@@ -21,7 +21,7 @@ function Front(){
 							
 					</Col>
 					<Col sm={5} className = "Image">
-						<img src="https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/80203944_2859807094043966_3896791748550066176_o.jpg?_nc_cat=100&_nc_sid=09cbfe&_nc_ohc=0xtyNsRWOykAX9-701U&_nc_ht=scontent-dfw5-2.xx&oh=d6d018cebbebc7b16db0aeddfbfb7ec5&oe=5F7C700A"></img>
+						<img src="/images/alex.jpg"></img>
 					</Col>
 				</Row>
 			</Container>
